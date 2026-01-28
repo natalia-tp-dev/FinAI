96c94118d71ff8d056883b2d351edcbe37c851ca
Natalia Triana Pulido <nataliatriana.p@gmail.com>
2026-01-27 22:14:47 -0500

    pushing final front for deploy

--- Front-end/FinAI-Front/src/environments/environment.development.ts
+++ Front-end/FinAI-Front/src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
     gateWayUrl: '/api',
-    paymentUrl: 'http://127.0.0.1:8081/api/payments',
     PLAN_PREMIUM_ID: 'P-3EP151961F1031623NFMHHUI',
     PLAN_ULTIMATE_ID: 'P-0VM12035A7244864HNFMHIKY'
 };
--- Front-end/FinAI-Front/src/environments/environment.ts
+++ Front-end/FinAI-Front/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    gateWayUrl: 'https://gateway-l8qg.onrender.com'
+    gateWayUrl: 'https://gateway-l8qg.onrender.com/api',
+    PLAN_PREMIUM_ID: 'P-3EP151961F1031623NFMHHUI',
+    PLAN_ULTIMATE_ID: 'P-0VM12035A7244864HNFMHIKY'
 };
