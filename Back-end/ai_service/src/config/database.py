591070846c8e11a622b442838b26d7e6fc03ea06
Natalia Triana Pulido <nataliatriana.p@gmail.com>
2026-01-27 20:23:56 -0500

    pushing final requirements.txt

--- Back-end/ai_service/src/config/database.py
+++ Back-end/ai_service/src/config/database.py
@@ -10,10 +10,13 @@ DB_USER = os.getenv('DB_AI_MODEL_USER')
 DB_PASS = os.getenv('DB_AI_MODEL_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_AI_MODEL_NAME')
+DB_PORT = os.getenv('DB_PORT')
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"ssl": {"ca": "/etc/ssl/certs/ca-certificates.crt"}}
+)
 SesionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
